4c6964cea5cc6f8143df920a72c61a246bfc3231
zhiyu1998 <542716863@qq.com>
2022-02-25 19:50:57 +0800

    :snail: 更新笔记
    1. 更新JVM笔记到堆
    2. 更新leetcode刷题笔记

--- LeetCode-Notes/Test/Test.java
+++ LeetCode-Notes/Test/Test.java
@@ -1,8 +1,20 @@
 public class Test {
 
     public static void main(String[] args) {
-        for (int i = 1; i < 3; i++) {
-            System.out.println(i);
+        int i = new Test().numTrees(9);
+        System.out.println(i);
+    }
+
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for( int i = 2; i < n + 1; i++) {
+            for ( int j = 1; j < i + 1; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
         }
+        return dp[n];
     }
 }
